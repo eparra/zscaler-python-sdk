5ef990a8ef472a58405538dc021d9d027294ec29
Eddie Parra <eparra@users.noreply.github.com>
2018-09-04 00:02:14 -0700

    Create Ssl.py

---
+++ zscaler_python_sdk/Ssl.py
@@ -0,0 +1,25 @@
+import logging
+from .Defaults import *
+
+
+class Ssl(object):
+
+
+	def delete_ssl_certchain(self):
+		pass
+
+	def download_csr(self):
+		pass
+
+	def generate_csr(self):
+		pass
+
+	def show_cert(self):
+		pass	
+
+	def upload_signed_cert(self):
+		pass	
+
+	def upload_cert_chain(self):
+		pass	
+    
