bfba04a2395474d26e4a3d0698ceff48af00bc0c
Eddie Parra <eparra@users.noreply.github.com>
2018-09-03 23:53:28 -0700

    Create Security.py

---
+++ zscaler_python_sdk/Security.py
@@ -0,0 +1,24 @@
+
+import logging
+
+class Security(object):
+
+
+	def get_whitelist_urls(self):
+		pass
+
+	def update_whitelist_urls(self):
+		pass
+
+	def get_blacklist_urls(self):
+		pass
+
+	def update_blacklist_urls(self):
+		pass	
+
+	def add_blacklist_urls(self):
+		pass	
+
+	def remove_blacklist_urls(self):
+		pass	
+        
