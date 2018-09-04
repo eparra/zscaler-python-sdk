5ce43cd649db783e189d642cee79a4d15acd3433
Eddie Parra <eparra@users.noreply.github.com>
2018-09-04 00:02:42 -0700

    Create User.py

---
+++ zscaler_python_sdk/User.py
@@ -0,0 +1,38 @@
+
+import logging
+from .Defaults import *
+
+
+class User(object):
+
+
+	def get_departments(self):
+		pass
+
+	def get_departments_by_id(self, department_id):
+		pass
+
+	def get_groups(self):
+		pass
+
+	def get_group_by_id(self, group_id):
+		pass
+
+	def get_users(self):
+		pass
+
+	def get_user_by_id(self, user_id):
+		pass
+
+	def create_user(self, user):
+		pass	
+
+	def delete_users(self, users):
+		pass	
+
+	def update_user(self, user):
+		pass	
+
+	def delete_user_by_id(self, user):
+		pass	
+    
