64a7d0b88b649ea001f980b3f25c7404ce1d9f29
Eddie Parra <eparra@users.noreply.github.com>
2020-01-08 16:41:30 -0800

    Update sandbox_md5_hash_query.py

--- examples/sandbox_md5_hash_query.py
+++ examples/sandbox_md5_hash_query.py
@@ -14,24 +14,30 @@ def main():
 	z.authenticate_zia_api()
 
         # Query Known Benign MD5 hash
-	#print("\n\n ##########  Query Known Benign MD5 hash  ##########\n\n")
-	#query1 = z.get_sanbox_report_md5('d732eef37d941722c8b23221bf173161')
+	print("\n\n ##########  Query Known Benign MD5 hash  ##########\n\n")
+	query1 = z.get_sanbox_report_md5('d732eef37d941722c8b23221bf173161')
 
 	# Query Known Malicious MD5 hash (summary)
-	#print("\n\n ##########  Query Known Malicious MD5 hash - summary report  ##########\n\n")
-	#query2 = z.get_sanbox_report_md5_summary('1acd57deafcbc29617a15708f45c31f0')
+	print("\n\n ##########  Query Known Malicious MD5 hash - summary report  ##########\n\n")
+	query2 = z.get_sanbox_report_md5_summary('1acd57deafcbc29617a15708f45c31f0')
 
 	# Query Known Malicious MD5 hash (full report)
 	print("\n\n ##########  Query Known Malicious MD5 hash - full report  ##########\n\n")
 	query3 = z.get_sanbox_report_md5('1acd57deafcbc29617a15708f45c31f0')
 
 	# Parse report using is_md5hash_malicious method to determine if file is malicious 
-	print("\n\n ##########  Parse report output to detemine if file is Malicious Query Known Malicious  ##########\n\n")
+	print("\n\n ##########  Parse report output to detemine if file is Malicious  ##########\n\n")
 	if z.is_md5hash_malicious(query3):
 		print("The File Is Malicious\n\n")
 	else:
 		print("The File Is Not Malicious\n\n")
 
+	# Parse report using is_md5hash_suspicious method to determine if file is malicious 
+	print("\n\n ##########  Parse report output to detemine if file is Suspicious  ##########\n\n")
+	if z.is_md5hash_suspicious(query3):
+		print("The File Is Suspicious\n\n")
+	else:
+		print("The File Is Not Suspicious\n\n")		
 
 if __name__ == '__main__':
 	main()
