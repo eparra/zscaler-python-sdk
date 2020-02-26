d570744e100fb4652491f8a5c660a4c08ff54b07
Vivek <vivek@Viveks-MacBook-Pro.local>
2020-02-26 15:07:45 +0530

    added get_sub_locations(), create_location_by_payload() methods in Locations.py

--- zscaler_python_sdk/Locations.py
+++ zscaler_python_sdk/Locations.py
@@ -15,9 +15,24 @@ class Locations(object):
 		return data['id']
 
 
-	def get_locations(self):
+	def get_locations(self, page_size=1000, page_count=1):
 
-		uri = self.api_url + 'api/v1/locations'
+		uri = self.api_url + f'api/v1/locations?pageSize={page_size}&page={page_count}'
+
+		res = self._perform_get_request(
+			uri,
+			self._set_header(self.jsessionid)
+		)
+		return res
+
+
+	def get_sub_locations(self, location_id, page_size=1000, page_count=1):
+
+		if not location_id:
+			return 'Location ID Required'
+
+		uri = self.api_url + \
+			  f'api/v1/locations/{location_id}/sublocations?pageSize={page_size}&page={page_count}'
 
 		res = self._perform_get_request(
 			uri,
@@ -29,6 +44,9 @@ class Locations(object):
 	def create_location(self, location_name, vpn_cred_id, fqdn, gateway_options = None):
 
 		uri = self.api_url + 'api/v1/locations'
+
+		if not location_name:
+			return 'Location Name Required'
 
 		if not vpn_cred_id:
 			return 'VPN Credential ID Required'
@@ -90,6 +108,59 @@ class Locations(object):
 		return res
 
 
+	def create_location_by_payload(self, z_loc_payload, is_sub_loc=False):
+		"""
+        Create location or sub-location by passing the correct payload.
+        Use this method to pass constructed zscaler location payload.
+
+        * To create zscaler location, these fields are mandatory:
+
+          * name
+          * vpnCredentials = [{},{}...]
+
+        * To create zscaler sub-location, these fields are mandatory:
+
+          * parentId
+          * name
+          * ipAddresses
+          * vpnCredentials = must be none or empty
+
+        For other non-mandatory fields, see - https://help.zscaler.com/zia/api#/Locations/addLocation
+
+        :param z_loc_payload: zscaler location payload
+        :param is_sub_loc: To create zscaler sub-location pass it True
+        """
+		if not (z_loc_payload and isinstance(z_loc_payload, dict)):
+			return 'Location Payload Required'
+
+		if not z_loc_payload.get('name', None):
+			return 'Location Name Required'
+
+		vpn_creds = z_loc_payload.get('vpnCredentials', [])
+
+		if not is_sub_loc and not vpn_creds:
+			return 'Location VPN Credentials Required'
+
+		if is_sub_loc:
+			if not z_loc_payload.get('parentId', None):
+				return 'Sub-Location Parent Id Required'
+
+			if not z_loc_payload.get('ipAddresses', []):
+				return 'Sub-Location Ip Addresses Required'
+
+			if vpn_creds:
+				return 'Sub-Location VPN Credentials Not Required'
+
+		uri = self.api_url + 'api/v1/locations'
+
+		res = self._perform_post_request(
+			uri,
+			z_loc_payload,
+			self._set_header(self.jsessionid)
+		)
+		return res
+
+
 	def get_locations_lite(self):
 
 		uri = self.api_url + 'api/v1/locations/lite' 
@@ -104,7 +175,7 @@ class Locations(object):
 	def get_locations_by_id(self, location_id):
 
 		if not location_id:
-			return "Location Requried"
+			return "Location Required"
 
 		uri = self.api_url + 'api/v1/locations/lite/' + str(location_id)
 
@@ -126,7 +197,7 @@ class Locations(object):
 	def get_vpn_endpoints(self, ipv4_addr):
 
 		if not ipv4_addr:
-			return 'IPv4 Address Requried'
+			return 'IPv4 Address Required'
 
 		uri = 'https://pac.zscalerbeta.net/getVpnEndpoints?srcIp=' + ipv4_addr
 
