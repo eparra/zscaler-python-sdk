847b08b0a7df50520c3ccc96ef3111caaee0e82f
Eddie Parra <ep@eddieparra.net>
2021-01-23 23:04:47 -0800

    Added methods

--- zscaler_python_sdk/UrlPolicies.py
+++ zscaler_python_sdk/UrlPolicies.py
@@ -6,16 +6,34 @@ class UrlPolicies(object):
 
 
 	def get_url_filtering_rules(self):
-		pass
+
+		uri = self.api_url + '/api/v1//urlFilteringRules'
+
+		res = self._perform_get_request(
+			uri,
+			self._set_header(self.jsessionid)
+		)
+		return res
+
 
 	def create_url_filtering_rules(self):
 		pass
 
-	def get_url_filtering_rules_by_id(self):
-		pass	
+
+	def get_url_filtering_rules_by_id(self, rule_id):
+
+		uri = self.api_url + '/api/v1/urlFilteringRules/' + str(category_id)
+
+		res = self._perform_get_request(
+			uri,
+			self._set_header(self.jsessionid)
+		)
+		return res		
+
 
 	def update_url_filtering_rules_by_id(self):
 		pass	
+
 
 	def delete_url_filtering_rules_by_id(self):
 		pass	
