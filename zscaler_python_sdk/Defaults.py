12915a1404b03cd09d13e29bd1181d5a612f021e
Eddie Parra <eparra@users.noreply.github.com>
2021-05-01 15:39:40 -0700

    Update Defaults.py
    Added MIN_API_KEY_LENGTH

--- zscaler_python_sdk/Defaults.py
+++ zscaler_python_sdk/Defaults.py
@@ -1,9 +1,10 @@
 global Z_CLOUDS, DEBUG_DEFAULT, MAX_FQDN_LEN, MAX_PSK_LEN, REQUEST_TIMEOUTS
 
-DEBUG_DEFAULT    = False
-MAX_FQDN_LEN     = 255
-MAX_PSK_LEN      = 64
-REQUEST_TIMEOUTS = (5, 25)
+DEBUG_DEFAULT      = False
+MAX_FQDN_LEN       = 255
+MAX_PSK_LEN        = 64
+MIN_API_KEY_LENGTH = 12
+REQUEST_TIMEOUTS   = (5, 25)
 
 Z_CLOUDS = {
 	'zscaler'      : 'https://zsapi.zscaler.net/',
