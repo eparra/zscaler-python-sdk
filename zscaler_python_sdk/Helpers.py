b427676956c4868337d2c1f81e36eed64994897c
Eddie Parra <eparra@users.noreply.github.com>
2021-05-09 23:14:53 -0700

    Update Helpers.py

--- zscaler_python_sdk/Helpers.py
+++ zscaler_python_sdk/Helpers.py
@@ -1,25 +1,40 @@
+
 import logging
+import json
 from .Defaults import *
 
 
 class Helpers(object):
 
-        def extract_values(self, obj, key):
-            """Recursively pull values of specified key from nested JSON."""
-            arr = []
 
-            def extract(obj, arr, key):
-                """Return all matching values in an object."""
-                if isinstance(obj, dict):
-                    for k, v in obj.items():
-                        if isinstance(v, (dict, list)):
-                            extract(v, arr, key)
-                        elif k == key:
-                            arr.append(v)
-                elif isinstance(obj, list):
-                    for item in obj:
-                        extract(item, arr, key)
-                return arr
+    def extract_values(self, obj, key):
 
-            results = extract(obj, arr, key)
-            return results
+        arr = []
+
+        def extract(obj, arr, key):
+
+            if isinstance(obj, dict):
+                for k, v in obj.items():
+                    if isinstance(v, (dict, list)):
+                        extract(v, arr, key)
+                    elif k == key:
+                        arr.append(v)
+            elif isinstance(obj, list):
+                for item in obj:
+                    extract(item, arr, key)
+            return arr
+
+        results = extract(obj, arr, key)
+        return results
+
+
+    def extract_id_from_response(self, json_response):
+
+        try:
+            data = json.loads(json_response)
+            if self.debug:
+                logging.debug("Extractd ID: {}".format(data['id']))
+            return data['id']
+        except:
+            logging.debug("Unable To Extract ID - EXITING\n\n")        
+            exit()
