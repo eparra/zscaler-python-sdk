ef2cc4cc059a93b8511d073797cbd8e0aa30156f
Eddie Parra <eparra@users.noreply.github.com>
2021-05-11 11:56:29 -0700

    Update Sandbox.py

--- zscaler_python_sdk/Sandbox.py
+++ zscaler_python_sdk/Sandbox.py
@@ -5,61 +5,64 @@ from .Defaults import *
 
 class Sandbox(object):
 
-	def _do_get_sanbox_report_md5(self, md5hash, report_type):
+    def _do_get_sanbox_report_md5(self, md5hash, report_type):
 
-		uri = self.api_url + 'api/v1/sandbox/report/' + str(md5hash)
+        uri = '{}api/v1/sandbox/report/{}'.format(
+            self.api_url,
+            str(md5hash)
+            )
 
-		if report_type is 'FULL':
-			uri += '?details=full'
-		else:
-			uri += '?details=summary'
+        if report_type == 'FULL':
+            uri += '?details=full'
+        else:
+            uri += '?details=summary'
 
-		res = self._perform_get_request(
-			uri,
-			self._set_header(self.jsessionid)
-		)
+        res = self._perform_get_request(
+            uri,
+            self._set_header(self.jsessionid)
+        )
 
-		if 'Retry-After' in res.json():
-			if self.debug:
-				logging.error("Zscaler RATE LIMIT REACHED")
-			return None
-		else:
-			return res.json()
-
-
-	def get_sanbox_report_md5(self, md5hash):
-
-		res = self._do_get_sanbox_report_md5(md5hash, 'FULL')
-		return res
+        if 'Retry-After' in res.json():
+            if self.debug:
+                logging.error("Zscaler RATE LIMIT REACHED")
+                return None
+        else:
+            return res.json()
 
 
-	def get_sanbox_report_md5_summary(self, md5hash):
+    def get_sanbox_report_md5(self, md5hash):
 
-		res = self._do_get_sanbox_report_md5(md5hash, 'SUMMARY')
-		return res
-
-
-	def get_sanbox_report_sha1(self):
-		pass
+        res = self._do_get_sanbox_report_md5(md5hash, 'FULL')
+        return res
 
 
-	def get_sanbox_report_sha256(self):
-		pass
+    def get_sanbox_report_md5_summary(self, md5hash):
+
+        res = self._do_get_sanbox_report_md5(md5hash, 'SUMMARY')
+        return res
 
 
-		def is_md5hash_suspicious(self, report):
+    def get_sanbox_report_sha1(self):
+        pass
 
-				extrated = (self.extract_values(report, 'Type'))
-				if 'SUSPICIOUS' in extrated:
-						return True
-				else:
-						return False
-		
-		
-	def is_md5hash_malicious(self, report):
 
-		extrated = (self.extract_values(report, 'Type'))
-		if 'MALICIOUS' in extrated:
-			return True
-		else:
-			return False
+    def get_sanbox_report_sha256(self):
+        pass
+
+
+    def is_md5hash_suspicious(self, report):
+
+        extrated = (self.extract_values(report, 'Type'))
+        if 'SUSPICIOUS' in extrated:
+            return True
+        else:
+            return False
+        
+
+    def is_md5hash_malicious(self, report):
+
+        extrated = (self.extract_values(report, 'Type'))
+        if 'MALICIOUS' in extrated:
+            return True
+        else:
+            return False
